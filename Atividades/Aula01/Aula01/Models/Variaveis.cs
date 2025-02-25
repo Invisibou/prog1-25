8bca883c21e3c972e602fe1e758caf93b188c672
429035 <429035@VDA-LCOII-19175>
2025-02-24 22:13:10 -0300

    Atividade Prog 1

---
+++ Atividades/Aula01/Aula01/Models/Variaveis.cs
@@ -0,0 +1,59 @@
+﻿namespace Aula01.Models
+{
+    public class Variaveis
+    {
+        //Tipos implícitos
+        //var teste = 10;
+
+        //Anotação de Tipos
+        public int userCount = 10;
+
+        //Uma variável pode ser declarada e não inicializada
+        public int totalCount;
+
+
+        //CONSTANTES
+        //PARA DECLARAR UMA CONSTANTE UTILIZAMOS A PALAVRA-CHAVE CONST
+        //No entanto, a cCONST deve ser inicilizada quando declarada
+
+        public const int interestRate = 10;
+        //O método construtor é invocado na instanciação do objeto por meio da palavra reservada new.
+        //Por regra, o construtor sempre tem o mesmo nome da classe
+
+        public byte minByte = 0;
+        public byte maxByte = 255;
+
+        public sbyte minSbyte = -128;
+        public sbyte maxSbyte = 127;
+
+        public short minShort = -32768;
+        public short maxShort = 32767;
+
+        public ushort minUshort = 0;
+        public ushort maxUshort = 65535;
+
+        public int minInt = -2147483648;
+        public int maxInt = 2147483647;
+
+        public uint minUint = 0;
+        public uint maxUint = 4294967295;
+
+        public long minLong = -9223372036854775808;
+        public long maxLong = 9223372036854775807;
+
+
+        // Parentêses é um procedimento
+        // Colchetes é matriz
+        // ; fim da sentença.
+        public Variaveis()
+        {
+            totalCount = 0;
+
+            //Tipo Implícito, você não informou se é tipo inteiro ou qualquer outro tipo, portanto, é implicito
+            //A palavra chave var se encarrega de definir o tipo da variável na instrução de atribuição
+            var signalStrength = 22;             
+            var companyName = "ACME";
+        }
+
+    }
+}
--- Atividades/Aula01/Aula01/obj/Debug/net9.0/Aula01.AssemblyInfo.cs
+++ Atividades/Aula01/Aula01/obj/Debug/net9.0/Aula01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdea9772a3c4abed48f5a5a0437a5bb4fd7be653")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
