5351c39fab00b9266fbe25274d7c21dee5622548
429035 <429035@VDA-LCOII-19175>
2025-06-09 22:19:01 -0300

    Prog-1-09.06.2025

---
+++ Aula06/Atividade-EstruturaMVC+Repository/Atividade-EstruturaMVC+Repository/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Atividade_EstruturaMVC_Repository.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
